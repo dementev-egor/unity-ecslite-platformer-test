ea89349c8622dae87189a711cc8edf0a555b88fb
Dementev Egor <d.egor.job@protonmail.com>
2022-06-08 11:43:39 +0300

    Ecs lib. Assets. Materials

---
+++ ecslite-platformer-test/Assets/Scripts/EcsLogic/GameController.cs
@@ -0,0 +1,30 @@
+﻿using Leopotam.EcsLite;
+
+namespace EcsLogic
+{
+    public class GameController
+    {
+        private EcsWorld _world;
+        private EcsSystems _systems;
+        
+        public void Initialize()
+        {
+            _world = new EcsWorld();
+            _systems = new EcsSystems(_world);
+        }
+
+        public void Run()
+        {
+            _systems?.Run();
+        }
+
+        public void Destroy()
+        {
+            _systems?.Destroy();
+            _systems = null;
+            
+            _world?.Destroy();
+            _world = null;
+        }
+    }
+}
---
+++ ecslite-platformer-test/Assets/Scripts/UnityPart/GameStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EcsLogic;
+using UnityEngine;
+
+namespace UnityPart
+{
+    public class GameStartup : MonoBehaviour
+    {
+        private GameController _gameController;
+
+        private void Awake()
+        {
+            _gameController = new GameController();
+            _gameController.Initialize();
+        }
+
+        private void Update()
+        {
+            _gameController.Run();
+        }
+
+        private void OnDestroy()
+        {
+            _gameController.Destroy();
+        }
+    }
+}
